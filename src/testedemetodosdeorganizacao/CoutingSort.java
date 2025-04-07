ddcaf277e1986bc13c170add5e02047ddf42ce04
Bryan Parreiras <bryanparreirascruz2005@gmail.com>
2025-04-07 14:33:14 -0300

    Adicionado método de encontrar maior elemento em um vetor
    Melhorado a logica geral dos métodos de organizar
    
    Criado o método de organização do Couting Sort

--- src/testedemetodosdeorganizacao/CoutingSort.java
+++ src/testedemetodosdeorganizacao/CoutingSort.java
@@ -1,5 +1,34 @@
 package testedemetodosdeorganizacao;
 
+import java.util.Arrays;
+
 public class CoutingSort {
+
+    public static void countingSort(int[] entrada, int[] saida, int tamanho, int valorMaximo) {
+        int[] contagem = new int[valorMaximo + 1];
+
+        // Etapa 1: Inicializa o vetor de contagem com zeros
+        for (int i = 0; i <= valorMaximo; i++) {
+            contagem[i] = 0;
+        }
+
+        // Etapa 2: Conta a frequência de cada valor no vetor de entrada
+        for (int j = 0; j < tamanho; j++) {
+            contagem[entrada[j]] = contagem[entrada[j]] + 1;
+        }
+
+        // Etapa 3: Soma cumulativa das contagens
+        for (int i = 1; i <= valorMaximo; i++) {
+            contagem[i] = contagem[i] + contagem[i - 1];
+        }
+
+        // Etapa 4: Monta o vetor de saída ordenado
+        for (int j = tamanho - 1; j >= 0; j--) {
+            saida[contagem[entrada[j]] - 1] = entrada[j];
+            contagem[entrada[j]] = contagem[entrada[j]] - 1;
+        }
+
+        System.out.println("vetor apos ordenação: " + "\n" + Arrays.toString(saida));
+    }
 
 }
--- src/testedemetodosdeorganizacao/MenuDeOrganizacao.java
+++ src/testedemetodosdeorganizacao/MenuDeOrganizacao.java
@@ -8,16 +8,15 @@ public class MenuDeOrganizacao {
 
     public void menuInicial() {
 
-        int[] tamanhoVetores = {100, 100};
+        int[] tamanhoVetores = {10};
 
         System.out.println("Seja bem vindo(a). Aperte enter para iniciar a medição!");
         Scanner teclado = new Scanner(System.in);
         teclado.nextLine();
         System.out.println("Iniciando os testes.");
-
-        menuRadixSort(tamanhoVetores);
-        menuBucketSort(tamanhoVetores);
         menuCoutingSort(tamanhoVetores);
+        // menuRadixSort(tamanhoVetores);
+        // menuBucketSort(tamanhoVetores);
 
     }
 
@@ -65,35 +64,93 @@ public class MenuDeOrganizacao {
     }
 
     public void menuCoutingSort(int[] tamanhoVetores) {
+        System.out.println("Couting Sort");
+        organizarCoutingCrescente(tamanhoVetores);
+        organizarCoutingDecrescente(tamanhoVetores);
+        organizarCoutingAleatorio(tamanhoVetores);
 
     }
 
-    public void organizarCoutingCrescente() {
+    public void organizarCoutingCrescente(int[] tamanhoVetores) {
+        System.out.println("Testando Couting Sort com vetor organizado crescente");
+
+        for (int i = 0; i < tamanhoVetores.length; i++) {
+            int[] vetorDesorganizado = preencheVetorCrescente(tamanhoVetores[i]);
+            int[] vetorOrganizado = new int[tamanhoVetores[i]];
+            int maiorElemento = acharMaiorElemento(vetorDesorganizado);
+            long tempoInicial = iniciaMedicaoTempo();
+            CoutingSort.countingSort(vetorDesorganizado, vetorOrganizado, vetorDesorganizado.length, maiorElemento);
+            finalizaMedicaoTempo(tempoInicial);
+        }
 
     }
 
-    public void organizarCoutingDecrescente() {
+    public void organizarCoutingDecrescente(int[] tamanhoVetores) {
+        System.out.println("Testando Couting Sort com vetor organizado decrescente");
+
+        for (int i = 0; i < tamanhoVetores.length; i++) {
+            int[] vetorDesorganizado = preencheVetorDecrescente(tamanhoVetores[i]);
+            int[] vetorOrganizado = new int[tamanhoVetores[i]];
+            int maiorElemento = acharMaiorElemento(vetorDesorganizado);
+            long tempoInicial = iniciaMedicaoTempo();
+            CoutingSort.countingSort(vetorDesorganizado, vetorOrganizado, vetorDesorganizado.length, maiorElemento);
+            finalizaMedicaoTempo(tempoInicial);
+        }
 
     }
 
-    public void organizarCoutingAleatorio() {
+    public void organizarCoutingAleatorio(int[] tamanhoVetores) {
+        System.out.println("Testando Couting Sort com vetor aleatorio");
 
+        for (int i = 0; i < tamanhoVetores.length; i++) {
+            int[] vetorDesorganizado = preencheVetorAleatorio(tamanhoVetores[i]);
+            int[] vetorOrganizado = new int[tamanhoVetores[i]];
+            int maiorElemento = acharMaiorElemento(vetorDesorganizado);
+            long tempoInicial = iniciaMedicaoTempo();
+            CoutingSort.countingSort(vetorDesorganizado, vetorOrganizado, vetorDesorganizado.length, maiorElemento);
+            finalizaMedicaoTempo(tempoInicial);
+        }
     }
 
     public void menuBucketSort(int[] tamanhoVetores) {
+        System.out.println("Couting Sort");
+        organizarBucketCrescente(tamanhoVetores);
+        organizarBucketDecrescente(tamanhoVetores);
+        organizarBucketAleatorio(tamanhoVetores);
+    }
+
+    public void organizarBucketCrescente(int[] tamanhoVetores) {
+        System.out.println("Testando Bucket Sort com vetor organizado crescente");
+
+        for (int i = 0; i < tamanhoVetores.length; i++) {
+            int[] vetorOrganizar = preencheVetorCrescente(tamanhoVetores[i]);
+            long tempoInicial = iniciaMedicaoTempo();
+            //chamo o método de organizar
+            finalizaMedicaoTempo(tempoInicial);
+        }
+    }
+
+    public void organizarBucketDecrescente(int[] tamanhoVetores) {
+        System.out.println("Testando Couting Sort com vetor organizado decrescente");
+
+        for (int i = 0; i < tamanhoVetores.length; i++) {
+            int[] vetorOrganizar = preencheVetorDecrescente(tamanhoVetores[i]);
+            long tempoInicial = iniciaMedicaoTempo();
+            //chamo o método de organizar
+            finalizaMedicaoTempo(tempoInicial);
+        }
 
     }
 
-    public void organizarBucketCrescente() {
+    public void organizarBucketAleatorio(int[] tamanhoVetores) {
+        System.out.println("Testando Couting Sort com vetor aleatorio");
 
-    }
-
-    public void organizarBucketDecrescente() {
-
-    }
-
-    public void organizarBucketAleatorio() {
-
+        for (int i = 0; i < tamanhoVetores.length; i++) {
+            int[] vetorOrganizar = preencheVetorAleatorio(tamanhoVetores[i]);
+            long tempoInicial = iniciaMedicaoTempo();
+            //chamo o método de organizar
+            finalizaMedicaoTempo(tempoInicial);
+        }
     }
 
     public int[] preencheVetorCrescente(int tamanhoVetor) {
@@ -102,7 +159,7 @@ public class MenuDeOrganizacao {
         for (int i = 0; i < tamanhoVetor; i++) {
             vetor[i] += i;
         }
-        
+
         System.out.println(Arrays.toString(vetor));
 
         return vetor;
@@ -122,7 +179,7 @@ public class MenuDeOrganizacao {
 
     }
 
-    public int [] preencheVetorAleatorio(int tamanhoVetor) {
+    public int[] preencheVetorAleatorio(int tamanhoVetor) {
         int[] vetor = new int[tamanhoVetor];
         long seed = 100L;
         Random sorteio = new Random(seed);
@@ -149,6 +206,18 @@ public class MenuDeOrganizacao {
         long duracaoTotal = finalizarMedicao - tempoInicial;
         System.out.println("Tempo de execução é igual a: "
                 + duracaoTotal / 1_000_000 + " ms");
+    }
+
+    private int acharMaiorElemento(int[] vetorDesorganizado) {
+        int maiorElemento = 0;
+        for (int i = 0; i < vetorDesorganizado.length; i++) {
+            if (vetorDesorganizado[i] > maiorElemento) {
+                maiorElemento = vetorDesorganizado[i];
+            }
+        }
+
+        return maiorElemento;
+
     }
 
 }
